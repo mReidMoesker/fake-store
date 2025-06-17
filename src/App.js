f67ff70bf9b1e31c72c463dd2b5c36b8904787ed
Duan <daniel.w2301@gmail.com>
2025-06-16 21:25:27 -0500

    split css files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './css/index.css';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
 import Footer from './components/Footer';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import './css/header.css';
+import './css/footer.css';
+import './css/notfound.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
