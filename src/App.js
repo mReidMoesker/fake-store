09b0c2925ec1e843f0b9ae4f2581a648cdeaadc8
mReidMoesker <opqetch@gmail.com>
2025-06-18 09:11:04 -0500

    added page to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import ShoppingCart from './pages/ShoppingCart';
 import Home from './pages/Home';
+import ProductPage from './pages/ProductPage';
 
 import './css/index.css';
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/5" element={<ProductPage/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
